c7b90b45459bbcbc9fecfbe79779dbdd235ad6f9
AlbionOfDarkness <JaretCr@yandex.ru>
2024-03-29 08:03:38 +0400

    add sorts

---
+++ func/menu/menu_sorts.c
@@ -0,0 +1,305 @@
+#include "../../header.h"
+
+void srt_menu_0(void)
+{
+	system("cls");
+	printf("\nThere are are the options that are bottom of this message:");
+	
+	printf("\n\t   1. Sort matrix paralell LOW->UP side by SelectionSort");
+	printf("\n\t   2. Sort main diagoonal UP->LOW by BubbleSort");
+	printf("\n\t   3. Sort array struct mark mediana UP->LOW by Selection(edit)");
+	printf("\n\t   4. Sort array structs num_group then s_name by InsertSort(edit)");
+	printf("\n\t   5. ShellSort");
+	printf("\n\t   6. QuickSort");
+	printf("\n\t   7. CountingSort");
+	printf("\n\t   Esc. to close program");
+	
+	printf("\n\n\n\nYou can use '<' and '>' to navigation!");
+	printf("\nPress SPACE to confirm choice");
+}
+
+void srt_menu_1(void)
+{
+	system("cls");
+	printf("\nThere are are the options that are bottom of this message:");
+	
+	printf("\n\t>>>1. Sort matrix paralell UP->LOW side by ChoiceSort");
+	printf("\n\t   2. Sort main diagoonal UP->LOW by BubbleSort");
+	printf("\n\t   3. Sort array struct mark mediana UP->LOW by InsertSort");
+	printf("\n\t   4. Sort array structs num_group then s_name by StltctionSort(edit)");
+	printf("\n\t   5. ShellSort");
+	printf("\n\t   6. QuickSort");
+	printf("\n\t   7. CountingSort");
+	printf("\n\t   Esc. to close program");
+	
+	printf("\n\n\n\nYou can use '<' and '>' to navigation!");
+	printf("\nPress SPACE to confirm choice");
+}
+void srt_menu_2(void)
+{
+	system("cls");
+	printf("\nThere are are the options that are bottom of this message:");
+	
+	printf("\n\t   1. Sort matrix paralell UP->LOW side by ChoiceSort");
+	printf("\n\t>>>2. Sort main diagoonal UP->LOW by BubbleSort");
+	printf("\n\t   3. Sort array struct mark mediana UP->LOW by InsertSort");
+	printf("\n\t   4. Sort array structs num_group then s_name by StltctionSort(edit)");
+	printf("\n\t   5. ShellSort");
+	printf("\n\t   6. QuickSort");
+	printf("\n\t   7. CountingSort");
+	printf("\n\t   Esc. to close program");
+	
+	printf("\n\n\n\nYou can use '<' and '>' to navigation!");
+	printf("\nPress SPACE to confirm choice");
+}
+void srt_menu_3(void)
+{
+	system("cls");
+	printf("\nThere are are the options that are bottom of this message:");
+	
+	printf("\n\t   1. Sort matrix paralell UP->LOW side by ChoiceSort");
+	printf("\n\t   2. Sort main diagoonal UP->LOW by BubbleSort");
+	printf("\n\t>>>3. Sort array struct mark mediana UP->LOW by InsertSort");
+	printf("\n\t   4. Sort array structs num_group then s_name by StltctionSort(edit)");
+	printf("\n\t   5. ShellSort");
+	printf("\n\t   6. QuickSort");
+	printf("\n\t   7. CountingSort");
+	printf("\n\t   Esc. to close program");
+	
+	printf("\n\n\n\nYou can use '<' and '>' to navigation!");
+	printf("\nPress SPACE to confirm choice");
+}
+void srt_menu_4(void)
+{
+	system("cls");
+	printf("\nThere are are the options that are bottom of this message:");
+	
+	printf("\n\t   1. Sort matrix paralell UP->LOW side by ChoiceSort");
+	printf("\n\t   2. Sort main diagoonal UP->LOW by BubbleSort");
+	printf("\n\t   3. Sort array struct mark mediana UP->LOW by InsertSort");
+	printf("\n\t>>>4. Sort array structs num_group then s_name by StltctionSort(edit)");
+	printf("\n\t   5. ShellSort");
+	printf("\n\t   6. QuickSort");
+	printf("\n\t   7. CountingSort");
+	printf("\n\t   Esc. to close program");
+	
+	printf("\n\n\n\nYou can use '<' and '>' to navigation!");
+	printf("\nPress SPACE to confirm choice");
+}
+void srt_menu_5(void)
+{
+	system("cls");
+	printf("\nThere are are the options that are bottom of this message:");
+	
+	printf("\n\t   1. Sort matrix paralell UP->LOW side by ChoiceSort");
+	printf("\n\t   2. Sort main diagoonal UP->LOW by BubbleSort");
+	printf("\n\t   3. Sort array struct mark mediana UP->LOW by InsertSort");
+	printf("\n\t   4. Sort array structs num_group then s_name by StltctionSort(edit)");
+	printf("\n\t>>>5. ShellSort");
+	printf("\n\t   6. QuickSort");
+	printf("\n\t   7. CountingSort");
+	printf("\n\t   Esc. to close program");
+	
+	printf("\n\n\n\nYou can use '<' and '>' to navigation!");
+	printf("\nPress SPACE to confirm choice");
+}
+void srt_menu_6(void)
+{
+	system("cls");
+	printf("\nThere are are the options that are bottom of this message:");
+	
+	printf("\n\t   1. Sort matrix paralell UP->LOW side by ChoiceSort");
+	printf("\n\t   2. Sort main diagoonal UP->LOW by BubbleSort");
+	printf("\n\t   3. Sort array struct mark mediana UP->LOW by InsertSort");
+	printf("\n\t   4. Sort array structs num_group then s_name by StltctionSort(edit)");
+	printf("\n\t   5. ShellSort");
+	printf("\n\t>>>6. QuickSort");
+	printf("\n\t   7. CountingSort");
+	printf("\n\t   Esc. to close program");
+	
+	printf("\n\n\n\nYou can use '<' and '>' to navigation!");
+	printf("\nPress SPACE to confirm choice");
+}
+void srt_menu_7(void)
+{
+	system("cls");
+	printf("\nThere are are the options that are bottom of this message:");
+	
+	printf("\n\t   1. Sort matrix paralell UP->LOW side by ChoiceSort");
+	printf("\n\t   2. Sort main diagoonal UP->LOW by BubbleSort");
+	printf("\n\t   3. Sort array struct mark mediana UP->LOW by InsertSort");
+	printf("\n\t   4. Sort array structs num_group then s_name by StltctionSort(edit)");
+	printf("\n\t   5. ShellSort");
+	printf("\n\t   6. QuickSort");
+	printf("\n\t>>>7. CountingSort");
+	printf("\n\t   Esc. to close program");
+	
+	printf("\n\n\n\nYou can use '<' and '>' to navigation!");
+	printf("\nPress SPACE to confirm choice");
+}
+void srt_menu_8_esc(void)
+{
+	system("cls");
+	printf("\nThere are are the options that are bottom of this message:");
+	
+	printf("\n\t   1. Sort matrix paralell UP->LOW side by ChoiceSort");
+	printf("\n\t   2. Sort main diagoonal UP->LOW by BubbleSort");
+	printf("\n\t   3. Sort array struct mark mediana UP->LOW by InsertSort");
+	printf("\n\t   4. Sort array structs num_group then s_name by StltctionSort(edit)");
+	printf("\n\t   5. ShellSort");
+	printf("\n\t   6. QuickSort");
+	printf("\n\t   7. CountingSort");
+	printf("\n\t>>>Esc. to close program");
+	
+	printf("\n\n\n\nYou can use '<' and '>' to navigation!");
+	printf("\nPress SPACE to confirm choice");
+}
+
+void menu_sorts(void)
+{
+	char input_c_srt;
+	int choice_sorts = 0;
+	
+	while(1)
+	{
+		switch(choice_sorts)
+		{
+			case 0: 
+				srt_menu_0();
+				break;
+			case 1:
+				srt_menu_1();
+				break;
+			case 2:
+				srt_menu_2();
+				break;
+			case 3:
+				srt_menu_3();
+				break;
+			case 4:
+				srt_menu_4();
+				break;
+			case 5:
+				srt_menu_5();
+				break;
+			case 6:
+				srt_menu_6();
+				break;
+			case 7:
+				srt_menu_7();
+				break;
+			case 8:
+				srt_menu_8_esc();
+				break;
+			default:
+				srt_menu_0();
+				break;
+		}
+		
+		
+		input_c_srt = getch();
+		
+		switch(input_c_srt)
+		{
+			case '1':
+				system("cls");
+				matrix_sort_1();
+				break;
+			case '2':
+				system("cls");
+				matrix_sort_2();
+				break;
+			case '3':
+				system("cls");
+				data_sort_1();
+				break;
+			case '4':
+				system("cls");
+				data_sort_2();
+				break;
+			case '5':
+				system("cls");
+				shell_sort_menu();
+				break;
+			case '6':
+				system("cls");
+				quick_sort_menu();
+				break;
+			case '7':
+				system("cls");
+				count_sort_menu();
+				break;
+			case 27:
+				system("cls");
+				menu();
+				break;
+			case '.':
+				system("cls");
+				choice_sorts = choice_scroll_sort(choice_sorts, +1);
+				break;
+			case ',':
+				system("cls");
+				choice_sorts = choice_scroll_sort(choice_sorts, -1);
+				break;
+			case 32:
+				system("cls");
+				choice_entre_sort(choice_sorts);
+				break;
+			default: 
+				wrong_button();
+				break;
+				
+		}
+		
+		system("cls");
+	}
+}
+
+int choice_scroll_sort(int choice, int diff)
+{
+	if((choice + diff) > 8) return 1;
+	if((choice + diff) < 1) return 8;
+	return choice + diff;
+}
+
+
+void choice_entre_sort(int choice)
+{
+	switch(choice)
+		{
+			case 1:
+				system("cls");
+				matrix_sort_1();
+				break;
+			case 2:
+				system("cls");
+				matrix_sort_2();
+				break;
+			case 3:
+				system("cls");
+				data_sort_1();
+				break;
+			case 4:
+				system("cls");
+				data_sort_2();
+				break;
+			case 5:
+				system("cls");
+				shell_sort_menu();
+				break;
+			case 6:
+				system("cls");
+				quick_sort_menu();
+				break;
+			case 7:
+				system("cls");
+				count_sort_menu();
+				break;
+			case 8: 
+				system("cls");
+				menu();
+				break;
+			default:
+				srt_menu_0();
+		}
+}
